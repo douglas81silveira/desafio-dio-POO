4b3bad3755e32e288b1ee0e91f8a42c2bc8786ae
macbook <macbook@MacBook-Pro.local>
2023-04-14 18:39:58 -0300

    Interligando as classes de domínio

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import java.time.LocalDate;
 
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 public class Main {
@@ -11,17 +13,38 @@ public class Main {
         curso1.setCargaHoraria(8);
 
         Curso curso2 = new Curso();
-        curso1.setTitulo("Curso Java");
-        curso1.setDescricao("Descrição curso Java");
-        curso1.setCargaHoraria(8);
+        curso2.setTitulo("Curso JS");
+        curso2.setDescricao("Descrição curso JS");
+        curso2.setCargaHoraria(4);
 
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria Java");
         mentoria.setDescricao("Descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+        // System.out.println(curso1);
+        // System.out.println(curso2);
+        // System.out.println(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devDouglas = new Dev("Douglas");
+        devDouglas.inscreverBootcamp(bootcamp);
+
+        System.out.println("Conteúdos inscritos:" + devDouglas.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos:" + devDouglas.getConteudosConcluidos());
+        System.out.println("XP: " + devDouglas.calcularTotalXp());
+
+        System.out.println("Progrendindo...");
+        devDouglas.progredir();
+
+        System.out.println("Conteúdos inscritos:" + devDouglas.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos:" + devDouglas.getConteudosConcluidos());
+        System.out.println("XP: " + devDouglas.calcularTotalXp());
     }
 }
